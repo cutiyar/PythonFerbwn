800277c533de2d0ee9a1e586574ebf861a73426f
cutiyar <jwtiyar@gmail.com>
2019-05-10 02:23:37 +0300

    yekem

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print("chaky1")
+    print("chonn2")
+                           #kate python compile aka bangy function nakat yaksar acheta sar condition ,lam nmwnaya condition akay
+                           # tatbyq krd w hardw print akay ba dway yaka krd.
+if __name__ == '__main__':main()
+
+
+def main():
+    print("chaky3")
+print("chonn4")
+                           #lam nmwnaya chwnka am print a lasar function namawa w chwata condition aka boya ama axwenetawa enja acheta sar function aka la dway am print ya.
+if __name__ == '__main__':main()
