79114ef0eb6e9a0d719eee59a776f36753d058d7
cutiyar <jwtiyar@gmail.com>
2019-05-12 13:58:13 +0300

    faily nwe

---
+++ Variables.py
@@ -0,0 +1,25 @@
+def main():
+    para=30
+    print(para)
+    ch=float(30)
+    print(ch,type(ch)) # python xoy dazane zhmaraka ya nwsynaka float a ya inte ya string a boya pewyst naka pey blley xoy dayzane.
+                        # wa harwaha datwany float bgorry bo int ya ba 3aksawa .
+
+
+
+# lerawa basy string akain
+
+    naw= "chhon\nbashy"
+    print(naw,type(naw))
+
+    jmare=40
+    ghj="{} is high or {} is low ".format(jmare ,10) #lera python xoy zhmarakan dwa badway yak danastawa ba tasalswl waryan agret.
+    #  ghj="%s is high"% jmare   # ama la versiony pesh 3.5 wa akra haman 3amalyay sarawa.
+    print(ghj)
+
+
+
+
+
+
+if __name__ == '__main__':main()
