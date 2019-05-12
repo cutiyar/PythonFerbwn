f5342fa1eece11a2a63914fde76e9261be425af4
cutiyar <jwtiyar@gmail.com>
2019-05-12 14:02:21 +0300

    faily nwe

--- Variables.py
+++ Variables.py
@@ -2,10 +2,9 @@ def main():
     para=30
     print(para)
     ch=float(30)
-    print(ch,type(ch)) # python xoy dazane zhmaraka ya nwsynaka float a ya inte ya string a boya pewyst naka pey blley xoy dayzane.
-                        # wa harwaha datwany float bgorry bo int ya ba 3aksawa .
-
-
+    print(ch,type(ch))
+    # python xoy dazane zhmaraka ya nwsynaka float a ya inte ya string a boya pewyst naka pey blley xoy dayzane.
+    # wa harwaha datwany float bgorry bo int ya ba 3aksawa .
 
 # lerawa basy string akain
 
@@ -13,13 +12,13 @@ def main():
     print(naw,type(naw))
 
     jmare=40
-    ghj="{} is high or {} is low ".format(jmare ,10) #lera python xoy zhmarakan dwa badway yak danastawa ba tasalswl waryan agret.
+    ghj="{} is high or {} is low ".format(jmare ,10)
+    #lera python xoy zhmarakan dwa badway yak danastawa ba tasalswl waryan agret.
     #  ghj="%s is high"% jmare   # ama la versiony pesh 3.5 wa akra haman 3amalyay sarawa.
     print(ghj)
 
-
-
-
-
+    drezhy="chak chaky"
+    print(len(drezhy))  # bas jmaray pytakant pe alle
+    print(drezhy[3])    # lera pet alle 3 yam shwen ch pyteky tyaya
 
 if __name__ == '__main__':main()
