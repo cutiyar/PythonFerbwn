fde59ad5bd852d00fe7f31d7e6af91213526f13d
cutiyar <jwtiyar@gmail.com>
2019-05-12 13:31:48 +0300

    AS

---
+++ Objects.py
@@ -0,0 +1,25 @@
+# le python ta3amwl lagall object akre object ysh pek hatwa la 4 shty sereki
+# 1. Name , 2. value ,3.Id ,4.type
+
+age = 29
+print(age, type(age))
+
+roz = 22.22
+print(roz, type(roz))
+
+naw = "jwty"
+print(type(naw), naw)
+
+naw = "jwty"
+print(id(naw))
+
+
+# muatble w immutable
+# la object komallek haya mutable wata atwanyn bygorryn wak (list,dictionary)
+# immutable natwanyn ba hy4 shewaya value kany bgorryn wak (float,int,string,tuple)
+
+x=30
+id(x)
+
+y=60
+id(y)
