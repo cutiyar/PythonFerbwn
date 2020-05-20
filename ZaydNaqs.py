2fa789d87fefa93cb1a39829f95dbd4033cce6a4
Jwtiyar Neriman <jwtiyar@gmail.com>
2020-05-20 23:34:33 +0300

    New Test

---
+++ ZaydNaqs.py
@@ -0,0 +1,30 @@
+def zayd_naqs():
+    print ('+------'),
+
+def xat():
+    print('|      '),
+
+def dwbara_zayd_naqs():
+    zayd_naqs()
+    zayd_naqs()
+    print ('+')
+def dwbara_xat():
+    xat()
+    xat()
+    print ('|')
+
+def chwarbara_xat():
+    dwbara_xat()
+    dwbara_xat()
+    dwbara_xat()
+    dwbara_xat()
+
+def helly_sarawa_dwbara(): #ama helly yakamy sarawa drwst akat lagall ryzy | ka
+    dwbara_zayd_naqs()
+    chwarbara_xat()
+
+def anjam_hamwy(): #ama draw kay axyr drwst akat
+    helly_sarawa_dwbara()
+    helly_sarawa_dwbara()
+    dwbara_zayd_naqs() #ama away yakam functiona lerawa axyry draw kay +------+-----+ pe dwbara drwst dakretawa
+anjam_hamwy()
