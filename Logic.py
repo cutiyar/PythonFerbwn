d44a5b11fa86ad6e1006db4fcd0046488da2ec78
jwtiyar <jwtiyar@gmail.com>
2020-08-08 11:09:42 +0300

    چاککردنی Logic.py

--- Logic.py
+++ Logic.py
@@ -1,28 +1,28 @@
 def main():
-# Basy Logic la python w Condition.
-x = 5
-y = 12
-zero = x > y
-print(zero)
+    # Basy Logic la python w Condition.
+    x = 5
+    y = 12
+    zero = x > y
+    print(zero)
 
-yak= x==y
-print(yak)
+    yak= x==y
+    print(yak)
 
-dw= x is y # (is) barawrdy shwen dakat.
-print(dw)
+    dw= x is y # (is) barawrdy shwen dakat.
+    print(dw)
 
-se= x>y and x<y # la AND false w true anjamaky false
-print(se)
+    se= x>y and x<y # la AND false w true anjamaky false
+    print(se)
 
-chwar= x>y or x<y # har danayak true bet awysh dabeta true la OR
-print(chwar)
+    chwar= x>y or x<y # har danayak true bet awysh dabeta true la OR
+    print(chwar)
 
-b=1
-not(b)
-print(b)
-b=0
-not (b)
-print(b)
+    b=1
+    not (b)
+    print(b)
+    b=0
+    not (b)
+    print(b)
 
 
 if __name__ == '__main__':main()
