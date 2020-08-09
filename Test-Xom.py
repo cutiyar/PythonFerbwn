636b293c2005b3a58c8bf4fb6fbad82dee96312a
jwtiyar <jwtiyar@gmail.com>
2020-08-09 13:51:32 +0300

    edit

--- Test-Xom.py
+++ Test-Xom.py
@@ -1,4 +1,4 @@
-import datetime
+'''import datetime
 
 Dayk = input("tement bnwse: ")
 sall = datetime.datetime.now().year
@@ -7,7 +7,7 @@ print("tement {}, be dllnyayewe sally dahatww debyte {} be xembe".format(zhyan, 
 print("Leber ewe Gwe mede dnya")
 
 '''
-# Emesh danyeky tre her leser if w else w condition detwany aw se quote labay w run y bkayt.
+'''''# Emesh danyeky tre her leser if w else w condition detwany aw se quote labay w run y bkayt.
 def main():
     Tomen= input("Tement bnwse: ")  #hibb
     if (int(Tomen)>5 and int(Tomen)<10):
@@ -25,3 +25,15 @@ def main():
     print("Esta dadexretawa")
 
 if __name__ == '__main__':main() '''
+
+def main(): #Em nmwneye bo eweye bzany detwanret naw blok of code chendyn condition w functiony try tyabet. wekw
+    #if le naw if, yan break,while,for,in
+    #lew block of code detwanret chenha string w beshy try tyabet
+    Tomen= input("Tement bnwse: ")  #hibb
+    if (int(Tomen)>5 and int(Tomen)<10):
+        if(int(Tomen)>7):
+            print("to ma3qwl tri")
+        else:
+            print("to myaqeky ma3qwlyi")
+
+if __name__ == '__main__':main()
